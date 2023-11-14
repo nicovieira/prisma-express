3a8fcaee927c9e9bdb056a6ffa07271cf06c7429
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-14 12:15:41 -0300

    validate body

--- src/routes/application.routes.ts
+++ src/routes/application.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { ApplicationControllers } from "../controllers/application.controllers";
+import { ValidateBody } from "../middlewares/validateBody.middleware";
+import { applicationCreateSchema } from "../schemas/application.schemas";
 
 export const applicationRouter = Router();
 
 const applicationControllers = new ApplicationControllers();
 
-applicationRouter.post("/:id/applications", applicationControllers.create);
+applicationRouter.post("/:id/applications", ValidateBody.execute(applicationCreateSchema), applicationControllers.create);
 applicationRouter.get("/:id/applications", applicationControllers.findMany);
--- src/routes/opportunity.routes.ts
+++ src/routes/opportunity.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { OpportunityControllers } from "../controllers/opportunity.controllers";
 import { applicationRouter } from "./application.routes";
+import { ValidateBody } from "../middlewares/validateBody.middleware";
+import { opportunityCreateSchema, opportunityUpdateSchema } from "../schemas/opportunity.schemas";
 
 export const opportunityRouter = Router();
 
 const opportunityControllers = new OpportunityControllers();
 
-opportunityRouter.post("/", opportunityControllers.create);
+opportunityRouter.post("/", ValidateBody.execute(opportunityCreateSchema) ,opportunityControllers.create);
 opportunityRouter.get("/", opportunityControllers.findMany);
 opportunityRouter.get("/:id", opportunityControllers.findOne);
-opportunityRouter.patch("/:id", opportunityControllers.update);
+opportunityRouter.patch("/:id", ValidateBody.execute(opportunityUpdateSchema), opportunityControllers.update);
 opportunityRouter.delete("/:id", opportunityControllers.delete);
 
 opportunityRouter.use("/", applicationRouter);
