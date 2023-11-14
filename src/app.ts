834a720aa0679b12c3b325c656ea8288149be078
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-14 11:45:53 -0300

    handle errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
 import { opportunityRouter } from "./routes/opportunity.routes";
+import { HandleErrors } from "./middlewares/handleErrors.middleware";
 
 export const app = express();
 
@@ -9,3 +11,5 @@ app.use(helmet());
 app.use(json());
 
 app.use("/opportunities", opportunityRouter);
+
+app.use(HandleErrors.execute);
