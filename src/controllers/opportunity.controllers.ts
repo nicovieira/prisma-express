71a502edbbd9b4c01e2b135cc44d8acc936c94cf
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-14 10:32:48 -0300

    create opportunity

--- src/controllers/opportunity.controllers.ts
+++ src/controllers/opportunity.controllers.ts
@@ -1,23 +1,28 @@
 import { Request, Response } from "express";
+import { OpportunityServices } from "../services/opportunity.services";
 
 export class OpportunityControllers{
-    create(req: Request, res: Response){
+    async create(req: Request, res: Response){
+        const opportunityServices = new OpportunityServices();
+
+        const response = await opportunityServices.create(req.body);
+
+        res.status(201).json(response);
+    }
+
+    async findMany(req: Request, res: Response){
 
     }
 
-    findMany(req: Request, res: Response){
+    async findOne(req: Request, res: Response){
 
     }
 
-    findOne(req: Request, res: Response){
+    async update(req: Request, res: Response){
 
     }
 
-    update(req: Request, res: Response){
-
-    }
-
-    delete(req: Request, res: Response){
+    async delete(req: Request, res: Response){
         
     }
 }
