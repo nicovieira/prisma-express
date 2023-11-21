5c933e0204b504e886f342c1d91424dd6f8089b9
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-21 16:17:29 -0300

    dotenv part1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
 import "express-async-errors";
+import "dotenv/config";
 import express, { json } from "express";
 import helmet from "helmet";
 import { opportunityRouter } from "./routes/opportunity.routes";
 import { HandleErrors } from "./middlewares/handleErrors.middleware";
 
 export const app = express();
+
+console.log(process.env.EXAMPLE);
 
 app.use(helmet());
 
