42cf23670f2a5a539ff7c3f294640400bd0cff0c
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-22 16:16:20 -0300

    user-services

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-    id: z.number().positive(),
-    name: z.string().min(1),
-    email: z.string().email().min(1),
-    password: z.string().min(8)
-})
+   id: z.number().positive(),
+   name: z.string().min(1),
+   email: z.string().email().min(1),
+   password: z.string().min(8),
+});
 
 export type TUser = z.infer<typeof userSchema>;
 
@@ -20,3 +20,8 @@ export type TUserLoginBody = z.infer<typeof userLoginBodySchema>;
 export const userReturnSchema = userSchema.omit({ password: true });
 
 export type TUserReturn = z.infer<typeof userReturnSchema>;
+
+export type TUserLoginReturn = {
+   accessToken: string;
+   user: TUserReturn;
+};
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,17 +1,52 @@
 import { injectable } from "tsyringe";
-import { TUserLoginBody, TUserRegisterBody } from "../schemas/user.schemas";
+import {
+   TUserLoginBody,
+   TUserLoginReturn,
+   TUserRegisterBody,
+   TUserReturn,
+   userReturnSchema,
+} from "../schemas/user.schemas";
+import { prisma } from "../database/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { AppError } from "../errors/appError";
 
 @injectable()
-export class UserServices{
-    register(body: TUserRegisterBody){
+export class UserServices {
+   async register(body: TUserRegisterBody): Promise<TUserReturn> {
+      const hashPassword = await bcrypt.hash(body.password, 10);
 
-    }
+      const newUser = {
+         ...body,
+         password: hashPassword,
+      };
 
-    login(body: TUserLoginBody){
+      const user = await prisma.user.create({ data: newUser });
 
-    }
+      return userReturnSchema.parse(user);
+   }
 
-    getUser(email: string){
+   async login(body: TUserLoginBody): Promise<TUserLoginReturn> {
+      const user = await prisma.user.findFirst({ where: { email: body.email } });
 
-    }
+      if (!user) {
+         throw new AppError(404, "User not registered");
+      }
+
+      const compare = bcrypt.compare(body.password, user.password);
+
+      if (!compare) {
+         throw new AppError(403, "Email and password doesn't match");
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
+
+      return { accessToken: token, user: userReturnSchema.parse(user) };
+   }
+
+   async getUser(id: number): Promise<TUserReturn> {
+      const user = await prisma.user.findFirst({ where: { id } });
+
+      return userReturnSchema.parse(user);
+   }
 }
