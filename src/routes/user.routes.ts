a45cdd597e621f509979ed8b8f67b7c9542cb1bb
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-27 14:12:52 -0300

    email already registered middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,14 +5,18 @@ import { UserControllers } from "../controllers/user.controllers";
 import { ValidateBody } from "../middlewares/validateBody.middleware";
 import { userLoginBodySchema, userRegisterBodySchema } from "../schemas/user.schemas";
 import { ValidateToken } from "../middlewares/validateToken.middleware";
+import { IsEmailAlreadyRegistered } from "../middlewares/isEmailAlreadyRegistered.middleware";
 
 container.registerSingleton("UserServices", UserServices);
 const userControllers = container.resolve(UserControllers);
 
 export const userRouter = Router();
 
-userRouter.post("/", ValidateBody.execute(userRegisterBodySchema), (req, res) =>
-   userControllers.register(req, res)
+userRouter.post(
+   "/",
+   IsEmailAlreadyRegistered.execute,
+   ValidateBody.execute(userRegisterBodySchema),
+   (req, res) => userControllers.register(req, res)
 );
 userRouter.post("/login", ValidateBody.execute(userLoginBodySchema), (req, res) =>
    userControllers.login(req, res)
